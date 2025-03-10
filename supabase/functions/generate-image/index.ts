390d4d419449bd0679b4828eb07ee65286c6d584
Seu Nome <seu.email@exemplo.com>
2025-03-10 18:52:24 -0400

    feat: usa modelo personalizado do Replicate

--- supabase/functions/generate-image/index.ts
+++ supabase/functions/generate-image/index.ts
@@ -30,28 +30,15 @@ serve(async (req) => {
     }
     console.log('Token encontrado');
 
-    const response = await fetch('https://api.replicate.com/v1/predictions', {
+    const response = await fetch('https://api.replicate.com/v1/deployments/prometheus1337/pvo-gen-ai/predictions', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        version: "39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
         input: {
-          prompt,
-          width: 1024,
-          height: 1024,
-          refine: "expert_ensemble_refiner",
-          scheduler: "K_EULER",
-          lora_scale: 0.6,
-          num_outputs: 1,
-          guidance_scale: 7.5,
-          apply_watermark: false,
-          high_noise_frac: 0.8,
-          negative_prompt: "",
-          prompt_strength: 0.8,
-          num_inference_steps: 25
+          prompt
         }
       }),
     });
