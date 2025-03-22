6bb9023752ce974ab948bc40e19523544085919d
Seu Nome <seu.email@exemplo.com>
2025-03-22 17:21:47 -0400

    revert: remove nova estrutura de menu com dropdowns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import { Navigation } from './components/Navigation';
-import { AppRoutes } from './routes';
+import { Layout } from './components/Layout';
+import LoginPage from './components/LoginPage';
+import PrivateRoute from './components/PrivateRoute';
+import { RestrictedRoute } from './components/RestrictedRoute';
+import { ImageGenerator } from './components/ImageGenerator';
+import { DocsHub } from './components/DocsHub';
+import { ReportsDashboard } from './components/ReportsDashboard';
+import { AdminDashboard } from './components/AdminDashboard';
+import { EssayDashboard } from './components/EssayDashboard';
+import { EssayCreditLogs } from './components/EssayCreditLogs';
+import { RefundDashboard } from './components/RefundDashboard';
 
 const theme = extendTheme({
   styles: {
@@ -37,14 +46,97 @@ export function App() {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
-        <Router>
-          <Box minH="100vh" bg="gray.50">
-            <Navigation />
-            <Box p={4}>
-              <AppRoutes />
-            </Box>
-          </Box>
-        </Router>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            
+            {/* Rota padrão - redireciona para relatórios */}
+            <Route path="/" element={<Navigate to="/relatorios" replace />} />
+            
+            {/* Rotas protegidas */}
+            <Route
+              path="/relatorios"
+              element={
+                <PrivateRoute>
+                  <Layout>
+                    <ReportsDashboard />
+                  </Layout>
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path="/documentos"
+              element={
+                <RestrictedRoute allowedRoles={['admin', 'support', 'commercial', 'essay_director', 'designer']}>
+                  <Layout>
+                    <DocsHub />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            <Route
+              path="/images"
+              element={
+                <RestrictedRoute allowedRoles={['admin', 'designer']}>
+                  <Layout>
+                    <ImageGenerator />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            <Route
+              path="/admin"
+              element={
+                <RestrictedRoute allowedRoles={['admin']}>
+                  <Layout>
+                    <AdminDashboard />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            {/* Rotas de redação */}
+            <Route
+              path="/redacoes"
+              element={
+                <RestrictedRoute allowedRoles={['admin', 'essay_director']}>
+                  <Layout>
+                    <EssayDashboard />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            <Route
+              path="/redacoes/creditos"
+              element={
+                <RestrictedRoute allowedRoles={['admin', 'essay_director']}>
+                  <Layout>
+                    <EssayCreditLogs />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            {/* Rota de reembolsos */}
+            <Route
+              path="/reembolsos"
+              element={
+                <RestrictedRoute allowedRoles={['admin', 'support']}>
+                  <Layout>
+                    <RefundDashboard />
+                  </Layout>
+                </RestrictedRoute>
+              }
+            />
+
+            {/* Rota para páginas não encontradas - redireciona para relatórios */}
+            <Route path="*" element={<Navigate to="/relatorios" replace />} />
+          </Routes>
+        </BrowserRouter>
       </AuthProvider>
     </ChakraProvider>
   );
